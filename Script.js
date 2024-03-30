688532fde6d864e9f23a81a6271f5908b7e5eb6f
LargemouthB <mattdelgado35@gmail.com>
2024-03-30 00:47:48 -0700

    changes made to javascript file and form to test post request to API, it appears previous method was not working properly

--- Script.js
+++ Script.js
@@ -20,12 +20,32 @@ Amplify.configure({
 /*--------Database connection------------------------------------*/
 
 /*--------Pushing data to database from RequestServices page ------------ */
-//let appForm = document.getElementById("app-form");
+const appForm = document.getElementById("app-form");
 
-//appForm.addEventListener("submit", (e) => {
-//    e.preventDefault();
-//});
+appForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
+    const formData = new FormData(form);
+    const formObject = Object.formEntries(formData.entries());
+
+    const apiEndpoint = "https://36ktc6fo3k.execute-api.us-east-1.amazonaws.com";
+
+    fetch(apiEndpoint, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formObject),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Success:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+
+    /*
 //API function to grab and push the data to dynamodb
 var callAPI = (fname, lname, email, tel, datetimelocal, serv1, serv2, serv3, serv4, serv5, addnotes) =>  {
     //create header object
@@ -59,3 +79,16 @@ var callAPI = (fname, lname, email, tel, datetimelocal, serv1, serv2, serv3, ser
         .then(result => alert(JSON.parse(result).body))
         .catch(error => console.log('error', error));
 };
+,
+                callAPI(document.getElementById('fname').value,
+                    document.getElementById('lname').value,
+                    document.getElementById('email').value,
+                    document.getElementById('pnumber').value,
+                    document.getElementById('date').value,
+                    document.getElementById('serv1').value,
+                    document.getElementById('serv2').value,
+                    document.getElementById('serv3').value,
+                    document.getElementById('serv4').value,
+                    document.getElementById('serv5').value,
+                    document.getElementById('addnotes').value)
+*/
