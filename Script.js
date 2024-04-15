148bdea20f96952486392adb0fc1eb8713f4e3bb
AWebb5225 <alexander.webber5225@gmail.com>
2024-04-14 20:50:42 -0700

    asd

--- Script.js
+++ Script.js
@@ -32,7 +32,7 @@ function fetchDataAndDisplay() {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://36ktc6fo3k.execute-api.us-east-1.amazonaws.com/dev', true);
     xhr.setRequestHeader('Allow-Access-Origin', '*');
-    xhr.setRequestHeader('Access-Control-Allow-Methods', 'POST, GET, PUT');
+    xhr.setRequestHeader('Access-Control-Allow-Headers', 'POST, GET, PUT');
     xhr.onload = function () {
         if (xhr.status === 200) {
             const responseData = JSON.parse(xhr.responseText);
